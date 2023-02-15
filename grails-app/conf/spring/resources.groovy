9abdc4929192f3dbdf341953767381b297d6a322
Alexandre Vila <alexandrevn@alu.ufc.br>
2023-02-15 15:46:13 -0300

    adicionado o spring security

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import comum.UsuarioPasswordEncoderListener
+import demo.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    usuarioPasswordEncoderListener(UsuarioPasswordEncoderListener)
+    userPasswordEncoderListener(UserPasswordEncoderListener)
 }
--- grails-app/init/crud/grails/react/BootStrap.groovy
+++ grails-app/init/crud/grails/react/BootStrap.groovy
@@ -1,8 +1,19 @@
 package crud.grails.react
 
+import demo.Admin
+import demo.Role
+import demo.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def admin = new Admin(name: "Admin", username: "Admin", password: "Admin").save()
+
+        Role role = new Role(authority: "ROLE_ADMIN").save()
+
+        UserRole.create(admin, role, true)
+
     }
     def destroy = {
     }
